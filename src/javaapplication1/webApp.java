94f353f1990fa87e7630d1c2c1ac5dc1d514b14a
shrijaba <shrijanaidu.ba@gmail.com>
2017-04-20 21:02:24 +0530

    Rest Client

---
+++ src/javaapplication1/webApp.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author bashr
+ */
+public interface webApp {
+    String Appurl="http://api.openweathermap.org/data/2.5/weather?q=";
+    String abc2="APPID=393c63c4a3ba26f3d2500c22c45c37fb";
+    String Exception="Run Time Exception";
+}
